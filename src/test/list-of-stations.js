067c388cf470ad69d2556c307458032235908162
Stefan Gründel <sgruendel@users.noreply.github.com>
2017-03-25 18:36:28 +0100

    add tests for names.json

--- src/test/list-of-stations.js
+++ src/test/list-of-stations.js
@@ -4,33 +4,36 @@ const expect = require('chai').expect;
 const fs = require('fs');
 const pegelonline = require('../pegelonlineRestAPI');
 
-const stations = fs.readFileSync('../slot-LIST_OF_STATIONS.txt').toString().split("\n");
+const stations = fs.readFileSync('../slot-LIST_OF_STATIONS.txt').toString().split('\n');
 const uuids = require('../uuids.json');
+const names = require('../names.json');
 
 describe('REST-API helpers', function() {
-    describe('#getCurrentMeasurement()', function() {
 
-        stations.forEach(function(station) {
-            if (station) {
-                // skip trailing empty line
-                
-                it('should have a uuid for ' + station, function() {
-                    expect(uuids[station]).to.exist;
-                });
+    stations.forEach(function(station) {
+        if (station) {
+            // skip trailing empty line
 
-                // This shouldn't be run regularly as it's time consuming ...
-                // it('should give current measurement for ' + station, function(done) {
-                //     pegelonline.getCurrentMeasurement(uuids[station], function(err, result) {
-                //         if (err) {
-                //             expect(err.message).to.equal('Not Found');
-                //         } else {
-                //             expect(result.unit).to.be.a('string');
-                //             expect(result.currentMeasurement.value).to.be.a('number');
-                //         }
-                //         done();
-                //     });
-                // });
-            }
-        });
+            const uuid = uuids[station];
+            it('should have a uuid for ' + station, function() {
+                expect(uuid).to.exist;
+            });
+            it('should have a name for ' + station, function() {
+                expect(names[uuid]).to.exist;
+            });
+
+            // This shouldn't be run regularly as it's time consuming ...
+            // it('should give current measurement for ' + station, function(done) {
+            //     pegelonline.getCurrentMeasurement(uuids[station], function(err, result) {
+            //         if (err) {
+            //             expect(err.message).to.equal('Not Found');
+            //         } else {
+            //             expect(result.unit).to.be.a('string');
+            //             expect(result.currentMeasurement.value).to.be.a('number');
+            //         }
+            //         done();
+            //     });
+            // });
+        }
     });
 });
