0692e508bcaae1186c6687384b43f9cd89d7eb58
Stefan Gründel <stefan.gruendel@garmin.com>
2017-03-29 13:09:35 +0200

    Extended getStations() test to make sure all uuids are known in the list of stations.

--- src/test/pegelonlineRestAPI.js
+++ src/test/pegelonlineRestAPI.js
@@ -3,21 +3,28 @@
 var expect = require('chai').expect;
 var pegelonline = require('../pegelonlineRestAPI');
 
-describe('REST-API helpers', function() {
-    describe('#getStations()', function() {
-        it('should give all stations', function(done) {
-            pegelonline.getStations(function(err, result) {
+const names = require('../names.json');
+
+describe('pegelonline', () => {
+    describe('#getStations()', () => {
+        it('should return all stations', done => {
+            pegelonline.getStations((err, result) => {
                 expect(err).to.be.null;
                 expect(result).to.have.length.above(500);
-                // TODO refactor test expect(result[0]).to.include.keys('uuid');
+
+                result.forEach(station => {
+                    expect(station.uuid).to.be.a('string');
+                    expect(names[station.uuid]).to.exist;
+                });
+
                 done();
             });
         });
     });
 
-    describe('#getCurrentMeasurement()', function() {
-        it('should give current measurement for Würzburg', function(done) {
-            pegelonline.getCurrentMeasurement('915d76e1-3bf9-4e37-9a9a-4d144cd771cc', function(err, result) {
+    describe('#getCurrentMeasurement()', () => {
+        it('should give current measurement for Würzburg', done => {
+            pegelonline.getCurrentMeasurement('915d76e1-3bf9-4e37-9a9a-4d144cd771cc', (err, result) => {
                 expect(err).to.be.null;
                 expect(result.unit).to.be.a('string');
                 expect(result.currentMeasurement.value).to.be.a('number');
@@ -25,8 +32,8 @@ describe('REST-API helpers', function() {
             });
         });
 
-        it('should not find Eisingen', function(done) {
-            pegelonline.getCurrentMeasurement('Eisingen', function(err, result) {
+        it('should not find Eisingen', done => {
+            pegelonline.getCurrentMeasurement('Eisingen', (err, result) => {
                 expect(err.message).to.equal('Not Found');
                 expect(result).to.be.undefined;
                 done();
@@ -34,16 +41,16 @@ describe('REST-API helpers', function() {
         });
     });
 
-    describe('#getSmallImageUrl()', function() {
-        it('should give URL for Würzburg', function() {
-            var result = pegelonline.getSmallImageUrl('915d76e1-3bf9-4e37-9a9a-4d144cd771cc');
+    describe('#getSmallImageUrl()', () => {
+        it('should give URL for Würzburg', () => {
+            const result = pegelonline.getSmallImageUrl('915d76e1-3bf9-4e37-9a9a-4d144cd771cc');
             expect(result).to.be.a('string');
         });
     });
 
-    describe('#getLargeImageUrl()', function() {
-        it('should give URL for Würzburg', function() {
-            var result = pegelonline.getLargeImageUrl('915d76e1-3bf9-4e37-9a9a-4d144cd771cc');
+    describe('#getLargeImageUrl()', () => {
+        it('should give URL for Würzburg', () => {
+            const result = pegelonline.getLargeImageUrl('915d76e1-3bf9-4e37-9a9a-4d144cd771cc');
             expect(result).to.be.a('string');
         });
     });
