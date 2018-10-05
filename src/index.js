eb792066682d1070a808785223ac98f07ee5161b
Stefan Gründel <stefan.gruendel@garmin.com>
2018-10-05 15:26:31 +0200

    Only use Dashbot.io on AWS.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 const Alexa = require('ask-sdk-core');
 const i18n = require('i18next');
 const sprintf = require('i18next-sprintf-postprocessor');
-const dashbot = require('dashbot')(process.env.DASHBOT_API_KEY).alexa;
+const dashbot = process.env.DASHBOT_API_KEY ? require('dashbot')(process.env.DASHBOT_API_KEY).alexa : undefined;
 
 const manager = require('./manager');
 const util = require('./util');
@@ -230,15 +230,15 @@ const LocalizationInterceptor = {
     },
 };
 
-exports.handler = dashbot.handler(
-    Alexa.SkillBuilders.custom()
-        .addRequestHandlers(
-            QueryStationIntentHandler,
-            QueryWaterIntentHandler,
-            HelpIntentHandler,
-            CancelAndStopIntentHandler,
-            SessionEndedRequestHandler)
-        .addRequestInterceptors(LocalizationInterceptor)
-        .addErrorHandlers(ErrorHandler)
-        .withSkillId(SKILL_ID)
-        .lambda());
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        QueryStationIntentHandler,
+        QueryWaterIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler)
+    .addRequestInterceptors(LocalizationInterceptor)
+    .addErrorHandlers(ErrorHandler)
+    .withSkillId(SKILL_ID)
+    .lambda();
+if (dashbot) exports.handler = dashbot.handler(exports.handler);
