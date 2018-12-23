693427a03ae6369762ae1b5df8b20ae1fb0212ac
Stefan Gründel <stefan.gruendel@gmail.com>
2018-12-23 16:44:49 +0100

    Use station Anderten to check for missing measurement.

--- test/unit/pegelonline.js
+++ test/unit/pegelonline.js
@@ -5,7 +5,7 @@ const pegelonline = require('../../src/pegelonline');
 
 describe('pegelonline', () => {
     describe('#getStations()', () => {
-        it('should return all stations', async function() {
+        it('should return all stations', async() => {
             const result = await pegelonline.getStations();
             expect(result).to.have.length.above(500);
 
@@ -16,7 +16,7 @@ describe('pegelonline', () => {
     });
 
     describe('#getWaters()', () => {
-        it('should return all waters', async function() {
+        it('should return all waters', async() => {
             const result = await pegelonline.getWaters();
             expect(result).to.have.length.above(90);
 
@@ -28,22 +28,22 @@ describe('pegelonline', () => {
     });
 
     describe('#getUuidsForWater()', () => {
-        it('should return UUID for Konstanz when querying Bodensee', async function() {
+        it('should return UUID for Konstanz when querying Bodensee', async() => {
             const result = await pegelonline.getUuidsForWater('bodensee');
             expect(result).to.eql(['aa9179c1-17ef-4c61-a48a-74193fa7bfdf']);
         });
     });
 
     describe('#getCurrentMeasurement()', () => {
-        it('should give current measurement for Würzburg', async function() {
+        it('should give current measurement for Würzburg', async() => {
             const result = await pegelonline.getCurrentMeasurement('915d76e1-3bf9-4e37-9a9a-4d144cd771cc');
             expect(result.unit).to.be.a('string');
             expect(result.currentMeasurement.value).to.be.a('number');
         });
 
-        it('should not find Eisingen', async function() {
+        it('should not find Anderten', async() => {
             try {
-                await pegelonline.getCurrentMeasurement('Eisingen');
+                await pegelonline.getCurrentMeasurement('98daae03-5aaa-4284-9717-7d52da4fe063');
             } catch (err) {
                 expect(err.statusCode).to.equal(404);
             }
