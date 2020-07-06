e1dd05032c8dc8d8b8c79564b928201a2db9831c
Stefan Gründel <stefan.gruendel@garmin.com>
2020-07-06 15:02:43 +0200

    Add Ahrenshoop as synonym for Althagen.

--- src/create-model.js
+++ src/create-model.js
@@ -52,7 +52,9 @@ function addStation(station, listOfStations, listOfVariants) {
         if (short.name !== long.name) {
             stationValue.name.synonyms = [ short.name ];
         }
-        if (short.name === 'Große Weserbrücke') {
+        if (short.name === 'Althagen') {
+            stationValue.name.synonyms = [ 'Ahrenshoop' ];
+        } else if (short.name === 'Große Weserbrücke') {
             stationValue.name.synonyms = [ 'Bremen' ];
         } else if (short.name === 'Hooksielplate') {
             stationValue.name.synonyms = [ 'Hooksiel' ];
