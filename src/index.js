bd73c6054cc33bc135a76e683cf47248773d2390
Stefan Gründel <stefan.gruendel@gmail.com>
2021-07-18 15:59:26 +0200

    Switch to APL.

--- src/index.js
+++ src/index.js
@@ -274,24 +274,47 @@ const QueryWaterLevelIntentHandler = {
             logger.info(cardContent);
 
             const title = 'Pegel bei ' + stationVariant;
-            if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope).Display) {
-                const measurementImage = new Alexa.ImageHelper()
-                    .withDescription('Wasserstandsdaten')
-                    .addImageInstance(result.image.xsmall.url, 'X_SMALL', result.image.xsmall.width, result.image.xsmall.height)
-                    .addImageInstance(result.image.medium.url, 'MEDIUM', result.image.medium.width, result.image.medium.height)
-                    .getImage();
-                const textContent = new Alexa.PlainTextContentHelper()
-                    .withPrimaryText(speechOutput)
-                    .withSecondaryText(measurementTime)
-                    .getTextContent();
+            if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']) {
+                const document = {
+                    type: 'APL',
+                    version: '1.6',
+                    import: [
+                        {
+                            name: 'alexa-layouts',
+                            version: '1.3.0',
+                        },
+                    ],
+                    mainTemplate: {
+                        parameters: [
+                            'detailTemplateData',
+                        ],
+                        item: [
+                            {
+                                type: 'AlexaDetail',
+                                headerTitle: '${detailTemplateData.headerTitle}',
+                                primaryText: '${detailTemplateData.primaryText}',
+                                secondaryText: '${detailTemplateData.secondaryText}',
+                                imageSource: '${detailTemplateData.imageSource}',
+                            },
+                        ],
+                    },
+                };
+                const datasources = {
+                    detailTemplateData: {
+                        headerTitle: title,
+                        primaryText: speechOutput,
+                        secondaryText: measurementTime,
+                        imageSource: result.image.medium.url,
+                    },
+                };
                 handlerInput.responseBuilder
-                    .addRenderTemplateDirective({
-                        type: 'BodyTemplate2',
-                        backButton: 'HIDDEN',
-                        image: measurementImage,
-                        title: title,
-                        textContent: textContent,
+                    .addDirective({
+                        type: 'Alexa.Presentation.APL.RenderDocument',
+                        version: '1.1',
+                        document,
+                        datasources,
                     });
+
             }
             return handlerInput.responseBuilder
                 .speak(speechOutput)
