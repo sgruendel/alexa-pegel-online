26c18d85d2b952aef125bc592cf1c9c5453ec6e8
Stefan Gründel <stefan.gruendel@gmail.com>
2024-02-10 11:08:59 +0100

    use const not var

--- lambda/test/unit/utils.js
+++ lambda/test/unit/utils.js
@@ -266,28 +266,28 @@ describe('utils', () => {
 
     describe('#getTimeDesc()', () => {
         it('should format hours with leading zeroes', () => {
-            var today = new Date();
+            const today = new Date();
             today.setHours(8, 12);
             const result = utils.getTimeDesc(today, LOCALE);
             expect(result).to.equal('08:12');
         });
 
         it('should format minutes with leading zeroes', () => {
-            var today = new Date();
+            const today = new Date();
             today.setHours(12, 1);
             const result = utils.getTimeDesc(today, LOCALE);
             expect(result).to.equal('12:01');
         });
 
         it('should format 24h', () => {
-            var today = new Date();
+            const today = new Date();
             today.setHours(15, 0);
             const result = utils.getTimeDesc(today, LOCALE);
             expect(result).to.equal('15:00');
         });
 
         it('should format yesterday', () => {
-            var yesterday = new Date();
+            const yesterday = new Date();
             yesterday.setDate(yesterday.getDate() - 1);
             yesterday.setHours(8, 1);
             const result = utils.getTimeDesc(yesterday, LOCALE);
@@ -313,7 +313,7 @@ describe('utils', () => {
         });
 
         it('should format the day before yesterday', () => {
-            var dby = new Date();
+            const dby = new Date();
             dby.setDate(dby.getDate() - 2);
             dby.setHours(8, 1);
             const result = utils.getTimeDesc(dby, LOCALE);
